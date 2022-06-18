7205e98a8dda132be26c4241fbf44ed516975b1e
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-06-18 18:30:12 +0900

    一旦コミット

--- SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/ContentView.swift
@@ -9,11 +9,160 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Home()
     }
 }
 
+struct Home: View {
+    
+    @State var color = 0
+    var body: some View {
+        VStack {
+            ZStack(alignment: .top) {
+                VStack {
+                    Image(self.color == 0 ? "lamp1" : "lamp2")
+                        .resizable()
+                        .frame(height: 300)
+                    
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            self.color = 0
+                        }, label: {
+                            VStack(spacing: 8) {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color.yellow)
+                                        .frame(width: 20, height: 20)
+                                    
+                                    Circle()
+                                        .stroke(self.color == 0 ? Color.white : Color.clear, lineWidth: 2)
+                                        .frame(width: 30, height: 30)
+                                }
+                                Text("Yellow")
+                                    .foregroundColor(.white)
+                                    .font(.caption)
+                            }
+                        })
+                        
+                        
+                        Button(action: {
+                            self.color = 1
+                        }, label: {
+                            VStack(spacing: 8) {
+                                ZStack {
+                                    Circle()
+                                        .fill(Color.orange)
+                                        .frame(width: 20, height: 20)
+                                    
+                                    Circle()
+                                        .stroke(self.color == 1 ? Color.white : Color.clear, lineWidth: 2)
+                                        .frame(width: 30, height: 30)
+                                }
+                                Text("Orange")
+                                    .foregroundColor(.white)
+                                    .font(.caption)
+                            }
+                        })
+                        
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 10)
+                }
+                
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image("back")
+                            .renderingMode(.original)
+                            .padding()
+                            
+                    })
+                    .padding(.leading, 10)
+                    .padding(.top, 20)
+
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image("cart")
+                            .renderingMode(.original)
+                            .padding()
+                    }).padding(.horizontal, 10)
+                        .padding(.vertical, 20)
+                        .background(Color.white)
+                        .clipShape(CustomShape(corner: .bottomLeft, radii: 35))
+                }
+            }
+            .background(self.color == 0 ? Color.yellow : Color.orange)
+            .clipShape(CustomShape(corner: .bottomLeft, radii: 55))
+            HStack {
+                Text("Melodi Lamp")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("heart")
+                        .renderingMode(.original)
+                        .padding()
+                })
+                .background(self.color == 0 ? Color.yellow : Color.orange)
+                .clipShape(Circle())
+                
+                
+            }
+            .padding(.horizontal, 35)
+            .padding(.top)
+            
+            Text("The lampshape provides directional lighting above the dining the dining table and pleasant diffused light throught the room")
+                .multilineTextAlignment(.leading)
+                .foregroundColor(.gray)
+                .padding(.horizontal, 30)
+                .padding(.top)
+            
+            HStack(spacing: 15) {
+                Button(action: {
+                    
+                }, label: {
+                    VStack {
+                        Image("mat1")
+                            .renderingMode(.original)
+                        
+                        Text("22 w")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+                })
+                .background(Color.black.opacity(0.06))
+                .cornerRadius(12)
+            }
+            .padding(.top, 20)
+            
+            Spacer(minLength: 0)
+        }
+        .edgesIgnoringSafeArea(.all)
+        .statusBar(hidden: true)
+        .animation(.default, value: 1)
+    }
+}
+
+struct CustomShape : Shape {
+    
+    var corner: UIRectCorner
+    var radii: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corner, cornerRadii: CGSize(width: radii, height: radii))
+        return Path(path.cgPath)
+    }
+    
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
