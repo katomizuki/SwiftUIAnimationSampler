0d94eb42159df1620b83882e55d231ca68143cf6
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-06-18 17:40:13 +0900

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by ミズキ on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
