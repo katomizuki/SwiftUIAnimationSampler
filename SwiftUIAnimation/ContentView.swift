c04914ac57d9c4a5ab2f305fa888bff3162046ee
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-06-18 19:07:03 +0900

    complete

--- SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
 struct Home: View {
     
     @State var color = 0
+    @State var height = UIScreen.main.bounds.height
+    @State var width = UIScreen.main.bounds.width
     var body: some View {
         VStack {
             ZStack(alignment: .top) {
@@ -90,61 +92,140 @@ struct Home: View {
                             .renderingMode(.original)
                             .padding()
                     }).padding(.horizontal, 10)
-                        .padding(.vertical, 20)
+                        .padding(.vertical,self.height > 800 ? 15 : 20)
                         .background(Color.white)
-                        .clipShape(CustomShape(corner: .bottomLeft, radii: 35))
+                        .clipShape(CustomShape(corner: .bottomLeft, radii: self.height > 800 ? 30 : 35))
                 }
             }
             .background(self.color == 0 ? Color.yellow : Color.orange)
             .clipShape(CustomShape(corner: .bottomLeft, radii: 55))
+            ScrollView(self.height > 800 ? .init() : .vertical , showsIndicators: false) {
+                VStack {
+                    HStack {
+                        Text("Melodi Lamp")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Image("heart")
+                                .renderingMode(.original)
+                                .padding()
+                        })
+                        .background(self.color == 0 ? Color.yellow : Color.orange)
+                        .clipShape(Circle())
+                        
+                        
+                    }
+                    .padding(.horizontal, 35)
+                    .padding(.top)
+                    
+                    Text("The lampshape provides directional lighting above the dining the dining table and pleasant diffused light throught the room")
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 30)
+                        .padding(.top)
+                    
+                    Spacer(minLength: 0)
+                    
+                    HStack(spacing: 10) {
+                        Button(action: {
+                            
+                        }, label: {
+                            VStack {
+                                Image("mat1")
+                                    .renderingMode(.original)
+                                
+                                Text("22 w")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            }
+                            .padding()
+                        })
+                        .background(Color.black.opacity(0.06))
+                        .cornerRadius(12)
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            VStack {
+                                Image("mat2")
+                                    .renderingMode(.original)
+                                
+                                Text("24 CM")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            }
+                            .padding()
+                        })
+                        .background(Color.black.opacity(0.06))
+                        .cornerRadius(12)
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            VStack {
+                                Image("mat3")
+                                    .renderingMode(.original)
+                                
+                                Text("26 CM")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            }
+                            .padding()
+                        })
+                        .background(Color.black.opacity(0.06))
+                        .cornerRadius(12)
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            VStack {
+                                Image("mat4")
+                                    .renderingMode(.original)
+                                
+                                Text("1.6 M")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            }
+                            .padding()
+                        })
+                        .background(Color.black.opacity(0.06))
+                        .cornerRadius(12)
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 25)
+                    
+                    Spacer(minLength: 0)
+                }
+            }
+            
+            
             HStack {
-                Text("Melodi Lamp")
+                Text("$12.99")
+                    .foregroundColor(.black)
                     .font(.title)
                     .fontWeight(.bold)
+                    .padding(.leading, 35)
+                    .padding(.bottom, 25)
                 
                 Spacer()
                 
                 Button(action: {
                     
                 }, label: {
-                    Image("heart")
-                        .renderingMode(.original)
-                        .padding()
+                    Text("Add to Cart")
+                        .foregroundColor(.black)
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 35)
                 })
                 .background(self.color == 0 ? Color.yellow : Color.orange)
-                .clipShape(Circle())
-                
-                
+                clipShape(CustomShape(corner: .topLeft, radii: 55))
             }
-            .padding(.horizontal, 35)
-            .padding(.top)
             
-            Text("The lampshape provides directional lighting above the dining the dining table and pleasant diffused light throught the room")
-                .multilineTextAlignment(.leading)
-                .foregroundColor(.gray)
-                .padding(.horizontal, 30)
-                .padding(.top)
-            
-            HStack(spacing: 15) {
-                Button(action: {
-                    
-                }, label: {
-                    VStack {
-                        Image("mat1")
-                            .renderingMode(.original)
-                        
-                        Text("22 w")
-                            .fontWeight(.bold)
-                            .foregroundColor(.black)
-                    }
-                    .padding()
-                })
-                .background(Color.black.opacity(0.06))
-                .cornerRadius(12)
-            }
-            .padding(.top, 20)
-            
-            Spacer(minLength: 0)
         }
         .edgesIgnoringSafeArea(.all)
         .statusBar(hidden: true)
@@ -162,6 +243,12 @@ struct CustomShape : Shape {
         return Path(path.cgPath)
     }
     
+}
+
+class Host: UIHostingController<ContentView> {
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
